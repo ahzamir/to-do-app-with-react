ce18d60447a581675f52607164579f5c17c28921
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-06-13 22:47:14 +0430

    add the functionalities to the main component so when the user edite the value of edit mode input it will update the state in main component

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -48,7 +48,7 @@ class TodoItem extends React.Component {
           className={styles.textInput}
           value={title}
           onChange={e => {
-            console.log(e.target.value, id)
+            this.props.setUpdate(e.target.value, id)
           }}
         />
       </li>
