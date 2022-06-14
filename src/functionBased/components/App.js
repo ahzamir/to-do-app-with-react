b219b76c03ae24c0ecb08349207ef9d2619110af
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-06-14 18:38:21 +0430

    add the navigation items

--- src/functionBased/components/App.js
+++ src/functionBased/components/App.js
@@ -2,13 +2,17 @@ import { Route, Routes } from 'react-router-dom';
 import About from '../pages/About';
 import TodoContainer from './TodoContainer';
 import NotMatch from '../pages/NotMatch';
+import Navbar from './Navbar';
 
 const App = () => (
-  <Routes>
-    <Route exact path="/" element={<TodoContainer />} />
-    <Route path="/about" element={<About />} />
-    <Route path="*" element={<NotMatch />} />
-  </Routes>
+  <>
+    <Navbar />
+    <Routes>
+      <Route exact path="/" element={<TodoContainer />} />
+      <Route path="/about" element={<About />} />
+      <Route path="*" element={<NotMatch />} />
+    </Routes>
+  </>
 );
 
 export default App;
